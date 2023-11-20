8f716e884f00cca6033b427101427820efbf45dc
Naomi Sano <nsano1@asu.edu>
2023-11-20 09:27:18 -0700

    try again

--- models/networks.py
+++ models/networks.py
@@ -135,7 +135,7 @@ class MeshConvNet(nn.Module):
         for i, ki in enumerate(self.k[:-1]):
             setattr(self, 'conv{}'.format(i), MResConv(ki, self.k[i + 1], nresblocks))
             setattr(self, 'norm{}'.format(i), norm_layer(**norm_args[i]))
-            #setattr(self, 'pool{}'.format(i), MeshPool(self.res[i + 1]))
+            setattr(self, 'pool{}'.format(i), norm_layer(**norm_args[i]))
             
         self.gp = torch.nn.AvgPool1d(self.res[-1])
         # self.gp = torch.nn.MaxPool1d(self.res[-1])
