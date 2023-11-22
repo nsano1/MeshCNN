8c36d0bfd7508c37ebc225c618c63531896d2c9c
Naomi Sano <nsano1@asu.edu>
2023-11-22 09:35:47 -0700

    centroid

--- models/networks.py
+++ models/networks.py
@@ -129,7 +129,7 @@ class MeshConvNet(nn.Module):
         super(MeshConvNet, self).__init__()
         self.k = [nf0] + conv_res
         self.res = [input_res] + pool_res
-        self.res = [input_res]
+        # self.res = [input_res]
         norm_args = get_norm_args(norm_layer, self.k[1:])
 
         for i, ki in enumerate(self.k[:-1]):
